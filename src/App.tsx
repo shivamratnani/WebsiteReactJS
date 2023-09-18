5e0629b1c7f6004da9e7bb47c77ee6149df5ba19
Shivam Ratnani <shivamratnani@Shivams-Air.lan>
2023-09-18 00:43:56 -0500

    recent commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
 
-function App() {
+import React, { useState } from 'react';
+import './App.css';
+import HomePage from './HomePage';
+import { ThemeContext, themes } from './themeContext';
+
+const App: React.FC = () => {
+  const [theme, setTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setTheme(theme === themes.dark ? themes.light : themes.dark);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" style={{ backgroundColor: theme.background, color: theme.foreground }}>
+        <HomePage />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
